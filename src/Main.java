9ef38c66dded90c0915d629d3930d957b37e67f1
Vanmathi138 <150342354+Vanmathi138@users.noreply.github.com>
2025-05-19 11:43:25 +0530

    part time wage for employee

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
                 "Program on Master Branch");
         EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
         employeeWageComputation.checkAttendance();
+        employeeWageComputation.partTimeEmployeeWage();
 
     }
 }
@@ -14,6 +15,7 @@ class EmployeeWageComputation {
     public static final int is_present = 1;
     public static final int wage_per_hour = 20;
     public static final int full_day_hour = 8;
+    public static final int part_time_wage = 8;
 
     void checkAttendance(){
         double randomValue = Math.floor(Math.random()*10)%2;
@@ -26,6 +28,18 @@ class EmployeeWageComputation {
         }else {
             System.out.println("Employee is absent");
             System.out.println("Daily Wage of Employee: ₹0");
+        }
+    }
+    void partTimeEmployeeWage(){
+        double randomValue = Math.floor(Math.random()*10)%2;
+        int attendance = (int) randomValue;
+        if (attendance ==  is_present){
+            int partTimeWage = part_time_wage * wage_per_hour;
+            System.out.println("Employee is present");
+            System.out.println("Part time wage of employee: "+partTimeWage);
+        }else {
+            System.out.println("Employee is absent");
+            System.out.println("Part time wage of employee: ₹0");
         }
     }
 }
